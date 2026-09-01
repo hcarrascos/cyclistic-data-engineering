c09e79b3d4b41cd0c8449746080d8a0a30d20902
Henrique Carrasco <hc.sevage@gmail.com>
2026-08-31 23:53:41 -0300

    Create Bronze trips table

---
+++ sql/bronze/01_create_bronze_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE raw.cyclistic_trips (
+    ride_id TEXT,
+    rideable_type TEXT,
+    started_at TIMESTAMP,
+    ended_at TIMESTAMP,
+    start_station_name TEXT,
+    start_station_id TEXT,
+    end_station_name TEXT,
+    end_station_id TEXT,
+    start_lat NUMERIC,
+    start_lng NUMERIC,
+    end_lat NUMERIC,
+    end_lng NUMERIC,
+    member_casual TEXT,
+    loaded_at TIMESTAMP DEFAULT NOW()
+);
+
